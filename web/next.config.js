2cd3e9a0799a0f3a4a283b9f966b4c4b188a78d3
Cody Brunner <me@codybrunner.com>
2022-07-03 23:45:46 -0500

    chore(web): things

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['components', 'hooks', 'layouts', 'pages', 'services', 'utils'],
+  },
   reactStrictMode: true,
 }
 
