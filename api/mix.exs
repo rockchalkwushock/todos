abe9106acdcf4fbcef53c40fcefbd0f994540e59
Cody Brunner <me@codybrunner.com>
2022-07-03 20:45:26 -0500

    chore(api): adds absinthe deps

--- api/mix.exs
+++ api/mix.exs
@@ -40,7 +40,10 @@ defmodule App.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      # GraphQL
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
