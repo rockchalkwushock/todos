f62986aeccf5f4bde75234fbcf5b0ac06c07b02e
Cody Brunner <me@codybrunner.com>
2022-08-21 18:07:46 -0500

    chore(api): updates deps

--- api/mix.exs
+++ api/mix.exs
@@ -33,10 +33,10 @@ defmodule App.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.10"},
+      {:phoenix, "~> 1.6.11"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, ">= 0.16.4"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
